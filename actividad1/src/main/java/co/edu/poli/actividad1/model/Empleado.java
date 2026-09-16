0e4137b8d5d964d878d889f37a92df63015a9e0a
allie <allie@Mariana>
2026-09-16 14:44:37 -0500

    POJO empleado

--- actividad1/src/main/java/co/edu/poli/actividad1/model/Empleado.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/model/Empleado.java
@@ -1,5 +1,71 @@
 package co.edu.poli.actividad1.model;
 
 public class Empleado {
+	
+	private Long idEmpleado;
+	
+	private String nombre;
+	
+	private String apellido;
+	
+	private String correo;
+	
+	private Long rol;
+
+	public Empleado(Long idEmpleado, String nombre, String apellido, String correo, Long rol) {
+		super();
+		this.idEmpleado = idEmpleado;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.correo = correo;
+		this.rol = rol;
+	}
+
+	public Long getIdEmpleado() {
+		return idEmpleado;
+	}
+
+	public void setIdEmpleado(Long idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public Long getRol() {
+		return rol;
+	}
+
+	public void setRol(Long rol) {
+		this.rol = rol;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [idEmpleado=" + idEmpleado + ", nombre=" + nombre + ", apellido=" + apellido + ", correo="
+				+ correo + ", rol=" + rol + "]";
+	}
+	
 
 }
