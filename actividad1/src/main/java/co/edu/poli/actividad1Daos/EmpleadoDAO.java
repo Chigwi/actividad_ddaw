b625deec6c2abe43f8c7d06ae14a3b1820f4e5f8
allie <allie@Mariana>
2026-09-16 14:55:17 -0500

    plantilla de DAO empleado

--- actividad1/src/main/java/co/edu/poli/actividad1Daos/EmpleadoDAO.java
+++ actividad1/src/main/java/co/edu/poli/actividad1Daos/EmpleadoDAO.java
@@ -1,9 +1,7 @@
 package co.edu.poli.actividad1Daos;
 
-import java.sql.Connection;
-
-
 import java.sql.*;
+import co.edu.poli.actividad1.model.Empleado;
 
 public class EmpleadoDAO {
 	
@@ -20,6 +18,31 @@ public class EmpleadoDAO {
 	public void setConnection(Connection connection) {
 		this.connection = connection;
 	}
+	
+	public String insert (Empleado e) {
+		
+	}
+	
+	public Empleado select (Long id) {
+		
+	}
+	
+	public List <Empleado> selectAll(){
+		
+	}
+	
+	public String Update (Empleado e) {
+		
+	}
+	
+	public String Delete (Long id) {
+		
+	}
+	
+	public Empleado mapRStoEmpleado(ResultSet rs) {
+		
+	}
+	
 	
 	
 	
