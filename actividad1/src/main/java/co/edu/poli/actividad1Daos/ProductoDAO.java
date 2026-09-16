c8479c151cdcf7769e75b353a574fb3152b2e75e
sysz <sysz@cachyos>
2026-09-16 16:42:17 -0500

    Metodo select y arreglos a insert en productoDAO.

--- actividad1/src/main/java/co/edu/poli/actividad1Daos/ProductoDAO.java
+++ actividad1/src/main/java/co/edu/poli/actividad1Daos/ProductoDAO.java
@@ -22,26 +22,48 @@ public class ProductoDAO {
 	}
 	
 	public String insert(Producto p) {
-		String sql = "SELECT * FROM \"producto\" WHERE \"id_producto\" = ?";
+		String sql = "INSERT INTO \"producto\"  (\"id_producto\", \"descripcion\", \"disponible\", \"precio\", \"categoria\") VALUES (?, ?, ?, ?, ?)";
 		
 		try (PreparedStatement pstmt = connection.prepareStatement(sql)){
-			pstmt.setString(1, p.getDescripcion());
-			pstmt.setBoolean(2, p.getDisponible());
-			pstmt.setDouble(3, p.getPrecio());
-			pstmt.setString(4, p.getCategoria());
+			pstmt.setLong(1, p.getIdProducto());
+			pstmt.setString(2, p.getDescripcion());
+			pstmt.setBoolean(3, p.getDisponible());
+			pstmt.setDouble(4, p.getPrecio());
+			pstmt.setString(5, p.getCategoria());
 			
 			pstmt.executeUpdate();
 			
 			return "Insercion exitosa!";
 		}catch(SQLException e) {
 			e.printStackTrace();
+			return null;
 		}
 		
-		return null;
+		
 	}
 	
 	public Producto select(String id) {
+		String sql = "SELECT * FROM \"producto\" WHERE \"id_producto\" = ?";
 		
+		try(PreparedStatement pstmt = connection.prepareStatement(sql)) {
+			
+			pstmt.setString(1, id);
+			ResultSet rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				
+				Producto p = new Producto(rs.getLong("id_producto"), rs.getString("descripcion"), rs.getBoolean("disponible"), rs.getDouble("precio"), rs.getString("categoria"));
+				
+				return p;
+			}else {
+				System.out.println("Usuario no encontrado");
+				return null;
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	public List<Producto> selectAll(){
