21bedf773e53cd6466a98b24d71fee19413cacbd
allie <allie@Mariana>
2026-09-16 17:47:34 -0500

    updated postgresql version

--- actividad1/src/main/java/co/edu/poli/actividad1/Main.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/Main.java
@@ -1,11 +1,25 @@
 package co.edu.poli.actividad1;
 
+import co.edu.poli.actividad1.model.Empleado;
+import co.edu.poli.actividad1Daos.EmpleadoDAO;
+
 /**
  *
  * @author Your name
  */
 public class Main {
-    public static void main(String... param) {
-        System.out.println("Starting application...");
+    public static void main(String [] param) {
+        
+    	EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+    	
+    	empleadoDAO.setConnection(DatabaseConnection.getInstance().getconConnection());
+    	
+    	Empleado Allie = new Empleado((long)1, "Allie", "Velandia", "allie.velandia@outlook.com", (long) 1);
+    	
+        empleadoDAO.insert(Allie);
+        
+        empleadoDAO.selectAll();
+        
+        
     }
 }
