2be44ff4e12ebc9c0907b80a12bb1a498e4afb4e
sysz <sysz@cachyos>
2026-09-16 14:49:46 -0500

    Creacion del pojo de producto.

---
+++ actividad1/src/main/java/co/edu/poli/actividad1/model/Producto.java
@@ -0,0 +1,56 @@
+package co.edu.poli.actividad1.model;
+
+public class Producto {
+	
+	private Long idProducto;
+	private String descripcion;
+	private Boolean disponible;
+	private Double precio;
+	private String categoria;
+	public Long getIdProducto() {
+		return idProducto;
+	}
+	public void setIdProducto(Long idProducto) {
+		this.idProducto = idProducto;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Boolean getDisponible() {
+		return disponible;
+	}
+	public void setDisponible(Boolean disponible) {
+		this.disponible = disponible;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public Producto(Long idProducto, String descripcion, Boolean disponible, Double precio, String categoria) {
+		super();
+		this.idProducto = idProducto;
+		this.descripcion = descripcion;
+		this.disponible = disponible;
+		this.precio = precio;
+		this.categoria = categoria;
+	}
+	@Override
+	public String toString() {
+		return "Producto [idProducto=" + idProducto + ", descripcion=" + descripcion + ", disponible=" + disponible
+				+ ", precio=" + precio + ", categoria=" + categoria + "]";
+	}
+	
+	
+
+}
--- actividad1/src/main/java/co/edu/poli/actividad1Daos/ProductoDAO.java
+++ actividad1/src/main/java/co/edu/poli/actividad1Daos/ProductoDAO.java
@@ -1,5 +1,24 @@
 package co.edu.poli.actividad1Daos;
 
-public class ProductoDAO {
+import java.sql.*;
 
+public class ProductoDAO {
+	private Connection connection;
+	
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public ProductoDAO(Connection connection) {
+		super();
+		this.connection = connection;
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public String insert(Producto p) {
+		
+	}
 }
