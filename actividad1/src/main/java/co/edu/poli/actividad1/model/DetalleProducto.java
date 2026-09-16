f274b50c7bc74786f4bcf78e13be7d12e02b36a0
salod <salod@salito.mshome.net>
2026-09-16 15:00:25 -0500

    firmas DetalleProductoDAO

--- actividad1/src/main/java/co/edu/poli/actividad1/model/DetalleProducto.java
+++ actividad1/src/main/java/co/edu/poli/actividad1/model/DetalleProducto.java
@@ -7,6 +7,55 @@ public class DetalleProducto {
 	private Long detallePedido;
 	private Long productoId;
 	
+	public DetalleProducto(Long detalleProducto, String notas, Long detallePedido, Long productoId) {
+		super();
+		this.detalleProducto = detalleProducto;
+		this.notas = notas;
+		this.detallePedido = detallePedido;
+		this.productoId = productoId;
+	}
+
+	public Long getDetalleProducto() {
+		return detalleProducto;
+	}
+
+	public void setDetalleProducto(Long detalleProducto) {
+		this.detalleProducto = detalleProducto;
+	}
+
+	public String getNotas() {
+		return notas;
+	}
+
+	public void setNotas(String notas) {
+		this.notas = notas;
+	}
+
+	public Long getDetallePedido() {
+		return detallePedido;
+	}
+
+	public void setDetallePedido(Long detallePedido) {
+		this.detallePedido = detallePedido;
+	}
+
+	public Long getProductoId() {
+		return productoId;
+	}
+
+	public void setProductoId(Long productoId) {
+		this.productoId = productoId;
+	}
+
+	@Override
+	public String toString() {
+		return "DetalleProducto [detalleProducto=" + detalleProducto + ", notas=" + notas + ", detallePedido="
+				+ detallePedido + ", productoId=" + productoId + "]";
+	}
+	
+	
+	
+	
 	
 
 }
--- actividad1/src/main/java/co/edu/poli/actividad1Daos/DetalleProductoDAO.java
+++ actividad1/src/main/java/co/edu/poli/actividad1Daos/DetalleProductoDAO.java
@@ -1,6 +1,8 @@
 package co.edu.poli.actividad1Daos;
 
+import co.edu.poli.actividad1.model.DetalleProducto;
 import java.sql.*;
+import java.util.List;
 
 public class DetalleProductoDAO {
 	
@@ -19,6 +21,27 @@ public class DetalleProductoDAO {
 		this.connection = connection;
 	}
 	
+	public String insert (DetalleProducto t) {
+		return  null;
+	}
 	
-
+	public DetalleProducto select (Long id) {
+		return null;
+	}
+	
+	public List<DetalleProducto> selctAll(){
+		return null;
+		
+	}
+	
+	public String Update (DetalleProducto e) {
+		return null;
+	}
+	
+	public String Delete (Long id) {
+		return null;
+	}
+	public DetalleProducto mapRStoDetalleProducto(ResultSet rs) {
+		return null;
+	}
 }
