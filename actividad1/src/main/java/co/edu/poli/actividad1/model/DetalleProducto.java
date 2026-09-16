5ced3dc9718c32c805fd108c6f8107786bdc8c95
salod <salod@salito.mshome.net>
2026-09-16 14:43:58 -0500

    creacion parcial pollo detalle producto y clase producto DAO

---
+++ actividad1/src/main/java/co/edu/poli/actividad1/model/DetalleProducto.java
@@ -0,0 +1,12 @@
+package co.edu.poli.actividad1.model;
+
+public class DetalleProducto {
+	
+	private Long detalleProducto;
+	private String notas;
+	private Long detallePedido;
+	private Long productoId;
+	
+	
+
+}
--- actividad1/src/main/java/co/edu/poli/actividad1Daos/DetalleProductoDAO.java
+++ actividad1/src/main/java/co/edu/poli/actividad1Daos/DetalleProductoDAO.java
@@ -1,6 +1,23 @@
 package co.edu.poli.actividad1Daos;
 
+import java.sql.*;
+
 public class DetalleProductoDAO {
+	
+	private Connection connection;
+	
+	public DetalleProductoDAO () {
+		
+		
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
 	
 	
 
