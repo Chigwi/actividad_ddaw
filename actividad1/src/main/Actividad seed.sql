b6b5212794976750ea35976ef846e16026dcc85a
allie <allie@Mariana>
2026-09-16 17:26:00 -0500

    sql seed

---
+++ actividad1/src/main/Actividad seed.sql
@@ -0,0 +1,23 @@
+
+CREATE TABLE empleado(
+    id_empleado int PRIMARY KEY,
+    nombre varchar(100) NOT NULL,
+    apellido varchar(100) NOT NULL,
+    correo varchar(100),
+    rol int
+);
+
+CREATE TABLE detalle_producto(
+    id_detalle_producto int PRIMARY KEY,
+    notas varchar(1000),
+    detalle_pedido int,
+    producto_id int
+);
+
+CREATE TABLE producto(
+    id_producto int PRIMARY KEY,
+    descripcion varchar(1000),
+    disponible boolean,
+    precio double,
+    categoria varchar(100)
+);
