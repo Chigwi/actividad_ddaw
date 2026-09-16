5f27a35bf10cc1c81cfcb91c20eba3614841dea7
salod <salod@salito.mshome.net>
2026-09-16 14:06:52 -0500

    proyecto base

---
+++ actividad1/src/main/java/co/edu/poli/actividad1/DatabaseConnection.java
@@ -0,0 +1,45 @@
+package co.edu.poli.actividad1;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+public class DatabaseConnection {
+	
+	private static DatabaseConnection instance;
+	
+	private Connection connection;
+	
+	private DatabaseConnection () {
+		
+		try {
+			
+			String url = "jdbc:postgresql://localhost:5432/nombre";
+					
+			String user = "user";
+			
+			String password = "Servidor123";
+			
+			connection = DriverManager.getConnection(url,user,password);
+	
+		}catch(SQLException e) {
+			e.printStackTrace();
+			
+		}
+	}
+	
+	public static DatabaseConnection getInstance() {
+		if(instance == null) {
+			instance = new DatabaseConnection();
+		}
+		return instance;
+	}
+	public Connection getconConnection() {
+		return connection;
+	}
+	
+	
+	
+
+}
